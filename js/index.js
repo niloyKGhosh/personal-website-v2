850b3779daed88f13622e5783f7d64aac8c766f4
niloyKGhosh <niloykumar256@gmail.com>
2020-06-17 09:21:02 -0400

    Used sass to deisgn the navbar

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 const nav = document.querySelector(".navbar");
 window.addEventListener("scroll", () => {
-	console.log(window.scrollY);
 	if (window.scrollY > nav.offsetHeight) {
-		nav.classList.add("bg-custom-scrolled");
+		nav.classList.add("navbar-bg-custom-scrolled");
 	} else {
-		nav.classList.remove("bg-custom-scrolled");
+		nav.classList.remove("navbar-bg-custom-scrolled");
 	}
 });
