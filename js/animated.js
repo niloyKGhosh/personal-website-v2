a659c1b7ca60b696c5c01ebbb65f6be1ec1ae7a8
niloyKGhosh <niloykumar256@gmail.com>
2020-06-19 20:34:10 -0400

    Added animation

--- js/animated.js
+++ js/animated.js
@@ -1,14 +1,29 @@
-const anim = anime.timeline({
-	loop: false,
-	direction: "forwards",
-});
+window.addEventListener("DOMContentLoaded", (e) => {
+	const anim = anime.timeline({
+		loop: false,
+		direction: "forwards",
+	});
 
-anim.add({
-	targets: "#hexagon",
-	strokeDashoffset: [anime.setDashoffset, 800],
-	easing: "linear",
-	duration: 3000,
-	delay: function (el, i) {
-		return i * 250;
-	},
+	anim.add({
+		targets: ".landing-page",
+		opacity: 1,
+		easing: "easeInQuad",
+		duration: 1800,
+	})
+		.add({
+			targets: "#hexagon",
+			opacity: 1,
+			easing: "easeInQuad",
+			duration: 200,
+		})
+		.add({
+			targets: "#hexagon",
+
+			strokeDashoffset: [anime.setDashoffset, 0],
+			easing: "linear",
+			duration: 2300,
+			delay: function (el, i) {
+				return i * 800;
+			},
+		});
 });
--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 const nav = document.querySelector(".navbar");
 const scrolled = document.querySelector(".navbar__bg-custom-scrolled");
+
 window.addEventListener("scroll", () => {
-	// console.log(nav.offsetHeight);
-	// console.log(scrolled);
+	console.log(nav.offsetHeight);
+	console.log(scrolled);
 	if (window.pageYOffset > 0) {
 		scrolled.style.opacity = 1;
 		nav.style.borderBottom = "0px";
