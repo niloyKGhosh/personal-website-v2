4bcddb1e5805ea05486ee7a38278355c0161f4d7
niloyKGhosh <niloykumar256@gmail.com>
2020-06-30 01:19:51 -0400

    Added dynamic footer date

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 const nav = document.querySelector(".navbar");
 const scrolled = document.querySelector(".navbar__bg-custom-scrolled");
+const footerYear = document.querySelector(".page-footer--year");
+const date = new Date();
+const currentYear = date.getFullYear();
 
+if (currentYear > 2020) {
+	footerYear.innerHTML = `2020-${currentYear}`;
+} else {
+	footerYear.innerHTML = `2020`;
+}
 window.addEventListener("scroll", () => {
-	console.log(nav.offsetHeight);
-	console.log(scrolled);
 	if (window.pageYOffset > 0) {
 		scrolled.style.opacity = 1;
 		nav.style.borderBottom = "0px";
